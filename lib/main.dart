a380040b9415fa18cfa5a1b6b7ee871e58a32ff2
Filipe Lauren <filipelauren@gmail.com>
2023-02-14 20:39:56 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Teste GIT',
+      title: 'Titulo alterado',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
