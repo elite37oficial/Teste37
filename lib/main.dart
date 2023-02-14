ac13df11b46a3dbcb0ac07c0102b5e075559c2ac
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-02-14 20:49:38 -0300

    tradução texto

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você pressionou o botão por:',
             ),
             Text(
               '$_counter',
